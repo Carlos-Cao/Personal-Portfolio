3df8ada1eb65323f6ca3b8a9f5623323c16def33
Carlos Cao <carlos.cao2@gmail.com>
2023-03-26 23:52:36 +1300

    feat: add page components

---
+++ components/About.tsx
@@ -0,0 +1,25 @@
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hello there, my name is Carlos Cao and I am a Developer based in
+        Auckland, New Zealand. I specialise in Backend Integration Development
+        and have a background in Computer Science. I am deeply passionate about
+        technology because of its limitless potential to create positive impact
+        in our lives. From automating tedious tasks to solving complex problems,
+        coding allows me to create solutions that make a difference. As a
+        Developer, I take pride in crafting efficient algorithms and debugging
+        complex software. I am always looking for ways to improve my skills and
+        stay up to date with the latest trends and technologies in the industry.
+        Whether I am collaborating with others or working independently, I bring
+        a sharp analytical mind and attention to detail to every project I work
+        on. I believe that technology is more than just a tool it is a driving
+        force behind innovation and progress.
+      </p>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default About;
---
+++ components/Main.tsx
@@ -0,0 +1,11 @@
+function Main() {
+  return (
+    <div>
+      <h1>Carlos Cao</h1>
+      <h2>Software Developer</h2>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default Main;
